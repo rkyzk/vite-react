570394e008823b4ece80b7e1404cb39db3c4fa56
rkyzk <reikoyazaki@yahoo.com>
2025-04-28 13:44:32 +0900

    add dependencies (materialUI, react icons, react-hot-toast)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import { FaBeer } from "react-icons/fa";
 
 function App() {
   return (
-    <div>
+    <div
+      className="flex items-center justify-center h-screen bg-gray-300
+      text-2xl front-bold"
+    >
       <p>Welcome!</p>
+      <FaBeer />
     </div>
   );
 }
