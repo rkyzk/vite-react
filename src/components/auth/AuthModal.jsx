8551249a199c334282bc65a2717e84647089c760
rkyzk <reikoyazaki@yahoo.com>
2025-12-20 20:34:49 +0900

    ログインダイアログの外をクリックしたらダイアログを閉じる

--- src/components/auth/AuthModal.jsx
+++ src/components/auth/AuthModal.jsx
@@ -1,9 +1,24 @@
 import Login from "./Login";
 import Register from "./Register";
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { closeModal } from "../../store/actions";
 
 const AuthModal = () => {
   const { loginOnly } = useSelector((state) => state.modal);
+  const dispatch = useDispatch();
+  /** ダイアログの外をクリックしたらダイアログを閉じる */
+  const handleCloseModal = (e) => {
+    if (e.target.classList.contains("MuiModal-backdrop")) {
+      dispatch(closeModal());
+      document.removeEventListener("mouseup", handleCloseModal);
+    }
+  };
+  useEffect(() => {
+    // 初回レンダーリングでイベントリスナーを追加
+    document.addEventListener("mouseup", (e) => handleCloseModal(e));
+  }, []);
+
   return (
     <div
       className="max-w-[450px] px-3 py-4 border-b-black bg-gray-50
