4287195301b8b9fdd31f9a571782f8fd6f3067bd
rkyzk <reikoyazaki@yahoo.com>
2026-02-07 13:53:15 +0900

    correct filename of productReducer.js

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 /** set the base URL for axios requests */
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL:
+    "http://ec2-54-250-2-23.ap-northeast-1.compute.amazonaws.com:8080/api",
+  // "http://localhost:8080/api",
   // "http://ec2-54-250-2-23.ap-northeast-1.compute.amazonaws.com:8080/api",
   // "http://ecommerce-rest-api-2025.ap-northeast-1.elasticbeanstalk.com/api",
   withCredentials: "true",
