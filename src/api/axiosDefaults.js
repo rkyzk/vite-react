74b59108cd9ad335c1e06e0c7f43a91e667165c3
rkyzk <reikoyazaki@yahoo.com>
2026-02-08 21:50:45 +0900

    production setting

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,11 +3,11 @@ import axios from "axios";
 /** set the base URL for axios requests */
 const api = axios.create({
   baseURL:
-    "http://ec2-54-250-2-23.ap-northeast-1.compute.amazonaws.com:8080/api",
-  // "http://localhost:8080/api",
+    "https://ec2-175-41-212-25.ap-northeast-1.compute.amazonaws.com:8080/api",
+  //  "http://localhost:8080/api",
   // "http://ec2-54-250-2-23.ap-northeast-1.compute.amazonaws.com:8080/api",
   // "http://ecommerce-rest-api-2025.ap-northeast-1.elasticbeanstalk.com/api",
-  withCredentials: "true",
+  // withCredentials: "true",
 });
 
 export default api;
