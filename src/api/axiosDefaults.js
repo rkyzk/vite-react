cbb98b9621f1b504f9584c7b55444ef33589a410
rkyzk <reikoyazaki@yahoo.com>
2026-03-01 16:10:09 +0900

    eliminate port number in baseURL

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 /** set the base URL for axios requests */
 const api = axios.create({
-  baseURL: "https://wild-blossom-garden.org:443/api",
+  baseURL: "https://wild-blossom-garden.org/api",
   //  "http://localhost:8080/api",
   // "http://ec2-54-250-2-23.ap-northeast-1.compute.amazonaws.com:8080/api",
   // "http://ecommerce-rest-api-2025.ap-northeast-1.elasticbeanstalk.com/api",
